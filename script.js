2599da4077cfad9c818432fdd88bd350191db317
fouad ajayi <fouadajayi6@gmail.com>
2022-11-12 22:24:30 +0100

    Digital clock

---
+++ script.js
@@ -0,0 +1,20 @@
+const clock = document.querySelector('.clock')
+
+let tick = () => {
+    
+    let now = new Date();
+
+    let h = now.getHours(); 
+    let m = now.getMinutes();
+    let s = now.getSeconds()
+
+    let html = `
+        <span> ${h} </span>
+        <span> ${m} </span>
+        <span> ${s} </span>
+    `
+
+    clock.innerHTML = html
+}
+
+setInterval(tick , 1000)

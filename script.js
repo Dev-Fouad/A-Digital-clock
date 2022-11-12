f27f7d2f4311717b31d66ff9a5066392d88f8ac4
fouad ajayi <fouadajayi6@gmail.com>
2022-11-12 22:38:43 +0100

    clock

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ let tick = () => {
     let s = now.getSeconds()
 
     let html = `
-        <span> ${h} </span>
-        <span> ${m} </span>
-        <span> ${s} </span>
+        <span> ${h} </span>:
+        <span> ${m} </span>:
+        <span> ${s} </span>:
     `
 
     clock.innerHTML = html
